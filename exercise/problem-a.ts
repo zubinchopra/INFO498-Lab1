995ba261720b18e268d4989830fd371644fa6fc3
Hao Chen <haoc46@cs.washington.edu>
2019-04-02 23:02:19 -0700

    Change types

--- exercise/problem-a.ts
+++ exercise/problem-a.ts
@@ -1,4 +1,4 @@
 // Given an array of ints a and an int k, return if there exists a pair (x, y) in a such that x + y = k.
-function hasPairSumToK(a, k) {
+function hasPairSumToK(a: string[], k:number): boolean {
 
 }
